a382a277f44d36f81ac09aa3450bcd06633dde1b
Vítor Messias <55415246+vittormessias@users.noreply.github.com>
2022-09-26 21:08:23 -0300

    exercicios finalizados em py

--- Algoritmo/ProjetoPython/ex031.py
+++ Algoritmo/ProjetoPython/ex031.py
@@ -1,16 +1,58 @@
-# No  Futebol  Americano,  usa-se  o Quarterback  Rating como  um  índice  que  indica  o desempenho  do quarterback(
-# quando  maior,  melhor).  Ele  é  calculado  como  indicado  a seguir:  Calcula-se  o  percentual  de  passes
-# completados  em  relação  aos  passes  tentados pelo quarterback. Deste valor subtrai-se 0,3 e divide-se por 0,
-# 2. Este valor não deve ser maior  que  2,375  ou  menor  que  0  (caso  seja,  ajusta-se  o  valor  para  2,
-# 375  ou  0, respectivamente). Em  seguida,  calcula-se  a  razão  de  jardas  passadas  pela  quantidade  de
-# passes  tentados. Deste valor, subtrai-se 3 e divide-se por 4. Novamente, este valor não deve ser maior que 2,
-# 375 ou menor que 0  (caso seja, procede-se como no caso anterior).Agora,  calcula-se  a  razão  de  passes  para
-# touchdowns pelo  número  de  passes  tentados. Divide-se  o  valor  por  0,05.  Mais  uma  vez,  este  valor  não
-# deve  ser  maior  que  2,375  ou menor que 0  (caso seja, procede-se como de costume). Então, calcula-se a razão
-# entre passes interceptados e o número de passes tentados. Deste valor,  subtrai-se  0,095  e  divide-se  o
-# resultado  por  0,04.  Como  de  praxe,  este  valor  não deve ser maior que 2,375 ou menor que 0  (caso seja,
-# atua-se como explicado).O quarterback   ratingé   calculando   somando-se   as   quatro parcelas anteriores,
-# multiplicando a soma por 100 e dividindo-se o produto por 6.Escreva  um  programa,  que  leia  o  número  de
-# passes  tentados,  o  número  de  passes completos, o número de jardas passadas, o número de passes para touchdowns
-# o número de passes interceptados e informe o QB Rating do quarterback.
+"""
+ATT = Passes tentados
+COMP = Passes completos
+YARDS = Jardas aéreas
+TD = Passes para touchdown
+INT = Passes interceptados
+"""
 
+# passos
+ATT = float(input('Passes tentados: '))
+COMP = float(input('Passes completos: '))
+YARDS = float(input('Jardas: '))
+TD = float(input('Passes para touchdown: '))
+INT = float(input('Passes interceptados: '))
+
+# Calcula-se  o  percentual  de  passes completados em relação aos passes tentados pelo quarterback
+A = ((COMP / ATT) - 0.3) * 5
+# caso  seja,  ajusta-se  o  valor  para  2,375  ou  0, respectivamente)
+if A > 2.375:
+    A = 2.375
+elif A < 0:
+    A = 0
+else:
+    A = A
+
+# calcula-se a razão de jardas passadas pela quantidade de passes tentados.
+B = ((YARDS / ATT) - 3) * 0.25
+# caso  seja,  ajusta-se  o  valor  para  2,375  ou  0, respectivamente)
+if B > 2.375:
+    B = 2.375
+elif B < 0:
+    B = 0
+else:
+    B = B
+
+# calcula-se a razão de passes para touchdowns pelo número de passes tentados
+C = (TD / ATT) * 20
+# caso  seja,  ajusta-se  o  valor  para  2,375  ou  0, respectivamente)
+if C > 2.375:
+    C = 2.375
+elif C < 0:
+    C = 0
+else:
+    C = C
+
+# Então, calcula-se a razão entre passes interceptados e o número de passes tentados.
+D = 2.375 - ((INT / ATT) * 25)
+# caso  seja,  ajusta-se  o  valor  para  2,375  ou  0, respectivamente)
+if D > 2.375:
+    D = 2.375
+elif D < 0:
+    D = 0
+else:
+    D = D
+
+# informe o QB Rating do quarterback
+passerRating = ((A + B + C + D) / 6) * 100
+print(f'QB Rating do quarterback: {passerRating:.1f}')
--- Algoritmo/ProjetoPython/ex032.py
+++ Algoritmo/ProjetoPython/ex032.py
@@ -1,2 +1,18 @@
 # Escreva um programa que leia um caractere diga se ele é uma vogal, consoante, número ou um símbolo (qualquer
 # outro caracter, que não uma letra ou número).
+
+print('*'*85)
+print('Esse programa diz se o valor digitado é uma vogal, consoante, número ou um símbolo')
+print('*'*85)
+caracter = input('Digite aqui: ').upper()[0]
+
+if caracter in 'AEIOU':
+    print('É uma vogal')
+elif caracter in 'BCDFGJKLMNPQRSTVWXZ':
+    print('É uma consoante')
+elif caracter in '12345678910':
+    print('É um número')
+elif caracter in '!@#$%¨&*()_+°/?=§´~^\|;,.':
+    print('É um símbolo')
+
+
